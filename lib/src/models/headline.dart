c6ffecda5ed21695eca5825818773fc7fcf462f8
fulleni <fulleni@hotmail.com>
2025-04-25 18:19:37 +0100

    feat(models): add headline model
    - Create Headline class to represent a news headline item
    - Implement JSON serialization and deserialization
    - Add properties for title, description, URL, image URL, publication date, source, and category
    - Include copyWith method for immutable updates
    - Generate headline.g.dart file for JSON serialization

---
+++ lib/src/models/headline.dart
@@ -0,0 +1,105 @@
+import 'package:equatable/equatable.dart';
+import 'package:ht_shared/src/models/category.dart';
+import 'package:ht_shared/src/models/source.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
+
+part 'headline.g.dart';
+
+/// {@template headline}
+/// Headline model
+///
+/// Represents a news headline item.
+/// {@endtemplate}
+@immutable
+@JsonSerializable()
+class Headline extends Equatable {
+  /// {@macro headline}
+  Headline({
+    required this.title,
+    this.description,
+    this.url,
+    this.imageUrl,
+    this.publishedAt,
+    this.source,
+    this.category,
+    String? id,
+  })  : assert(
+          id == null || id.isNotEmpty,
+          'id can not be null and should be empty',
+        ),
+        id = id ?? const Uuid().v4();
+
+  /// Factory method to create a [Headline] instance from a JSON map.
+  factory Headline.fromJson(Map<String, dynamic> json) =>
+      _$HeadlineFromJson(json);
+
+  /// Unique identifier for the headline.
+  final String id;
+
+  /// Title of the headline.
+  final String title;
+
+  /// Description or snippet of the headline content.
+  final String? description;
+
+  /// URL to the full article or content.
+  final String? url;
+
+  /// URL to an image associated with the headline.
+  @JsonKey(name: 'imageUrl')
+  final String? imageUrl;
+
+  /// Date and time when the headline was published.
+  @JsonKey(name: 'publishedAt')
+  final DateTime? publishedAt;
+
+  /// Source or origin of the headline.
+  final Source? source;
+
+  /// Category of the current headline.
+  final Category? category;
+
+  /// Converts this [Headline] instance to a JSON map.
+  Map<String, dynamic> toJson() => _$HeadlineToJson(this);
+
+  @override
+  List<Object?> get props => [
+    id,
+    title,
+    description,
+    url,
+    imageUrl,
+    publishedAt,
+    source,
+    category,
+  ];
+
+  @override
+  bool get stringify => true;
+
+  /// Creates a new [Headline] with updated properties.
+  /// Use this to modify a [Headline] without changing the original instance.
+  Headline copyWith({
+    String? id,
+    String? title,
+    String? description,
+    String? url,
+    String? imageUrl,
+    DateTime? publishedAt,
+    Source? source,
+    Category? category,
+  }) {
+    return Headline(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      url: url ?? this.url,
+      imageUrl: imageUrl ?? this.imageUrl,
+      publishedAt: publishedAt ?? this.publishedAt,
+      source: source ?? this.source,
+      category: category ?? this.category,
+    );
+  }
+}
