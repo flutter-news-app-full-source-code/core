46540d599bc0e599350a5e693234ad3f7e15d326
fulleni <fulleni@hotmail.com>
2025-05-03 09:17:41 +0100

    feat(exceptions): add new exception types
    - Add AuthenticationException
    - Add InvalidInputException
    - Add OperationFailedException

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,8 +1,11 @@
+export 'authentication_exception.dart';
 export 'bad_request_exception.dart';
 export 'forbidden_exception.dart';
 export 'ht_http_exception.dart';
+export 'invalid_input_exception.dart';
 export 'network_exception.dart';
 export 'not_found_exception.dart';
+export 'operation_failed_exception.dart';
 export 'server_exception.dart';
 export 'unauthorized_exception.dart';
 export 'unknown_exception.dart';
