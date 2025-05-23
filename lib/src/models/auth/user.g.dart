5eca01115a90edf743125aa11931dd54333e9a1c
fulleni <fulleni@hotmail.com>
2025-05-23 19:29:10 +0100

    refactor(user): update User model serialization
    - Use checkedCreate for deserialization
    - Add explicit toJson and includeIfNull
    - Field rename snake case

--- lib/src/models/auth/user.g.dart
+++ lib/src/models/auth/user.g.dart
@@ -6,18 +6,29 @@ part of 'user.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-User _$UserFromJson(Map<String, dynamic> json) => User(
-      id: json['id'] as String,
-      role: $enumDecode(_$UserRoleEnumMap, json['role']),
-      email: json['email'] as String?,
-      createdAt: _dateTimeFromJson(json['createdAt'] as String?),
+User _$UserFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'User',
+      json,
+      ($checkedConvert) {
+        final val = User(
+          id: $checkedConvert('id', (v) => v as String),
+          role:
+              $checkedConvert('role', (v) => $enumDecode(_$UserRoleEnumMap, v)),
+          email: $checkedConvert('email', (v) => v as String?),
+          createdAt: $checkedConvert(
+              'created_at', (v) => _dateTimeFromJson(v as String?)),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'createdAt': 'created_at'},
     );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'id': instance.id,
-      'email': instance.email,
+      if (instance.email case final value?) 'email': value,
       'role': _$UserRoleEnumMap[instance.role]!,
-      'createdAt': _dateTimeToJson(instance.createdAt),
+      if (_dateTimeToJson(instance.createdAt) case final value?)
+        'created_at': value,
     };
 
 const _$UserRoleEnumMap = {
