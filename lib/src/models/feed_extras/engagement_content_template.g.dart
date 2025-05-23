0bcb3dcf93af96aa14ef8428870807e34746bc33
fulleni <fulleni@hotmail.com>
2025-05-23 19:31:15 +0100

    feat: Enhance EngagementContentTemplate model
    - Added copyWith method
    - Added @immutable annotation
    - Improved serialization/deserialization

--- lib/src/models/feed_extras/engagement_content_template.g.dart
+++ lib/src/models/feed_extras/engagement_content_template.g.dart
@@ -8,11 +8,21 @@ part of 'engagement_content_template.dart';
 
 EngagementContentTemplate _$EngagementContentTemplateFromJson(
         Map<String, dynamic> json) =>
-    EngagementContentTemplate(
-      type: $enumDecode(_$EngagementTemplateTypeEnumMap, json['type']),
-      title: json['title'] as String,
-      description: json['description'] as String?,
-      callToActionText: json['callToActionText'] as String?,
+    $checkedCreate(
+      'EngagementContentTemplate',
+      json,
+      ($checkedConvert) {
+        final val = EngagementContentTemplate(
+          type: $checkedConvert(
+              'type', (v) => $enumDecode(_$EngagementTemplateTypeEnumMap, v)),
+          title: $checkedConvert('title', (v) => v as String),
+          description: $checkedConvert('description', (v) => v as String?),
+          callToActionText:
+              $checkedConvert('call_to_action_text', (v) => v as String?),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'callToActionText': 'call_to_action_text'},
     );
 
 Map<String, dynamic> _$EngagementContentTemplateToJson(
@@ -22,13 +32,13 @@ Map<String, dynamic> _$EngagementContentTemplateToJson(
       'title': instance.title,
       if (instance.description case final value?) 'description': value,
       if (instance.callToActionText case final value?)
-        'callToActionText': value,
+        'call_to_action_text': value,
     };
 
 const _$EngagementTemplateTypeEnumMap = {
-  EngagementTemplateType.rateApp: 'rate-app',
-  EngagementTemplateType.linkAccount: 'link-account',
-  EngagementTemplateType.upgradeToPremium: 'upgrade-to-premium',
-  EngagementTemplateType.completeProfile: 'complete-profile',
-  EngagementTemplateType.exploreNewFeature: 'explore-new-feature',
+  EngagementTemplateType.rateApp: 'rate_app',
+  EngagementTemplateType.linkAccount: 'link_account',
+  EngagementTemplateType.upgradeToPremium: 'upgrade_to_premium',
+  EngagementTemplateType.completeProfile: 'complete_profile',
+  EngagementTemplateType.exploreNewFeature: 'explore_new_feature',
 };
