c7b32b1be5a5ee8901f6e0ca8a76c0c4fcb8f5f2
fulleni <fulleni@hotmail.com>
2025-07-04 18:06:17 +0100

    Update dashboard_summary.dart
    Signed-off-by: fulleni <fulleni@hotmail.com>

--- lib/src/fixtures/dashboard_summary.dart
+++ lib/src/fixtures/dashboard_summary.dart
@@ -1,6 +1,6 @@
 /// Dashboard Summary Demo Data
 const dashboardSummaryFixtureData = {
-  'id': 'summary',
+  'id': 'dashboard_summary',
   'headline_count': 1234,
   'category_count': 56,
   'source_count': 78,
