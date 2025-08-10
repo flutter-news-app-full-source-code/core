362e91329aead66793e0eed015b43ce451f1602f
fulleni <fulleni@hotmail.com>
2025-08-10 07:09:33 +0100

    feat(lib): add ContentCollectionItem model
    - Create a new model class for representing a horizontally scrollable list of feed items
    - Use generics to allow for different types of feed items within the collection
    - Implement JsonSerializable for easy serialization and deserialization
    - Include properties for unique identifier, decorator type, title, and list of items

---
+++ lib/src/models/feed_decorators/content_collection_item.dart
@@ -0,0 +1,57 @@
+import 'package:core/src/enums/feed_decorator_type.dart';
+import 'package:core/src/models/core/feed_item.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part 'content_collection_item.g.dart';
+
+/// {@template content_collection_item}
+/// A feed item representing a horizontally scrollable list of other feed items.
+///
+/// This is used for decorators like suggesting topics or sources to follow.
+/// The generic type [T] must be a subtype of [FeedItem].
+/// {@endtemplate}
+@immutable
+@JsonSerializable(
+  genericArgumentFactories: true,
+  explicitToJson: true,
+  includeIfNull: true,
+  checked: true,
+)
+class ContentCollectionItem<T extends FeedItem> extends FeedItem {
+  /// {@macro content_collection_item}
+  const ContentCollectionItem({
+    required this.id,
+    required this.decoratorType,
+    required this.title,
+    required this.items,
+  }) : super(type: 'contentCollection');
+
+  /// Factory method to create a [ContentCollectionItem] instance from a JSON map.
+  factory ContentCollectionItem.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object? json) fromJsonT,
+  ) => _$ContentCollectionItemFromJson(json, fromJsonT);
+
+  /// Unique identifier for the item.
+  final String id;
+
+  /// The specific type of decorator this item represents.
+  final FeedDecoratorType decoratorType;
+
+  /// The title to display above the collection.
+  final String title;
+
+  /// The list of items to display in the collection.
+  final List<T> items;
+
+  @override
+  Map<String, dynamic> toJson(Object? Function(T value) toJsonT) {
+    final json = _$ContentCollectionItemToJson(this, toJsonT);
+    json['type'] = type;
+    return json;
+  }
+
+  @override
+  List<Object?> get props => [id, decoratorType, title, items, type];
+}
