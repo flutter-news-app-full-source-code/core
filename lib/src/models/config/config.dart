9f4d579081ad958ebfc66f13309e1938532b760d
fulleni <fulleni@hotmail.com>
2025-08-10 07:11:22 +0100

    refactor(models): reorganize and update models for feed decorators
    - Update enums exports: remove feed_action_type, add feed_decorator_category and feed_decorator_type
    - Update auth exports: replace user_feed_action_status with user_feed_decorator_status
    - Update config exports: add feed_decorator_settings and role_specific_settings, remove account_action_config
    - Update core exports: remove unused content_type enum
    - Update feed_decorators exports: add call_to_action_item and content_collection_item, remove feed_action

--- lib/src/models/config/config.dart
+++ lib/src/models/config/config.dart
@@ -1,5 +1,6 @@
-export 'account_action_config.dart';
 export 'ad_config.dart';
 export 'app_status.dart';
+export 'feed_decorator_settings.dart';
 export 'remote_config.dart';
+export 'role_specific_settings.dart';
 export 'user_preference_config.dart';
