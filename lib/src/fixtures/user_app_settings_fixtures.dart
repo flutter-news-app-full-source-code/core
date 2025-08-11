00c61e51b04c00ecdcf0b66de4799f32662bf1a2
fulleni <fulleni@hotmail.com>
2025-08-11 11:19:04 +0100

    refactor(fixtures): update imports to use core package
    - Replace individual enum and model imports with a single core package import
    - This change simplifies the import structure and improves maintainability

--- lib/src/fixtures/user_app_settings_fixtures.dart
+++ lib/src/fixtures/user_app_settings_fixtures.dart
@@ -1,13 +1,4 @@
-import 'package:core/src/enums/app_accent_theme.dart';
-import 'package:core/src/enums/app_base_theme.dart';
-import 'package:core/src/enums/app_font_weight.dart';
-import 'package:core/src/enums/app_text_scale_factor.dart';
-import 'package:core/src/enums/headline_density.dart';
-import 'package:core/src/enums/headline_image_style.dart';
-import 'package:core/src/fixtures/fixture_ids.dart';
-import 'package:core/src/models/user_settings/display_settings.dart';
-import 'package:core/src/models/user_settings/feed_display_preferences.dart';
-import 'package:core/src/models/user_settings/user_app_settings.dart';
+import 'package:core/core.dart';
 
 /// User App Settings Demo Data
 final List<UserAppSettings> userAppSettingsFixturesData = [
