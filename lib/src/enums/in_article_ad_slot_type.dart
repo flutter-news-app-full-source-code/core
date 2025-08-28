7281fdcf14f37d062b6ee6e253a9cad255ef0216
fulleni <fulleni@hotmail.com>
2025-08-28 17:13:11 +0100

    feat(enums): add InArticleAdSlotType enum
    Defines specific, standard locations for ads within an article's content,
    including positions below the main image, above/below the continue reading
    button.

---
+++ lib/src/enums/in_article_ad_slot_type.dart
@@ -0,0 +1,13 @@
+/// {@template in_article_ad_slot_type}
+/// Defines specific, standard locations for ads within an article's content.
+/// {@endtemplate}
+enum InArticleAdSlotType {
+  /// Ad placed directly below the main article image.
+  belowMainArticleImage,
+
+  /// Ad placed just before a "Continue Reading" button.
+  aboveArticleContinueReadingButton,
+  
+  /// Ad placed just after a "Continue Reading" button.
+  belowArticleContinueReadingButton,
+}
