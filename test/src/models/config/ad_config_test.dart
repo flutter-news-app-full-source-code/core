a3a15bb5287ba93178dee1261b1ba7c21e381e9e
fulleni <fulleni@hotmail.com>
2025-08-28 18:19:13 +0100

    test(core): add AdConfig model tests
    - Instantiate test for AdConfig
    - Equality test for AdConfig
    - CopyWith test for AdConfig
    - Json round trip test for AdConfig

---
+++ test/src/models/config/ad_config_test.dart
@@ -0,0 +1,47 @@
+import 'package:core/core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('AdConfig', () {
+    final adConfigFixture = remoteConfigsFixturesData.first.adConfig;
+
+    test('can be instantiated', () {
+      expect(adConfigFixture, isA<AdConfig>());
+      expect(adConfigFixture.primaryAdPlatform, AdPlatformType.admob);
+      expect(adConfigFixture.platformAdIdentifiers, isA<Map<AdPlatformType, AdPlatformIdentifiers>>());
+      expect(adConfigFixture.localAdsCatalog, isA<Map<String, LocalAd>>());
+      expect(adConfigFixture.feedAdConfiguration, isA<FeedAdConfiguration>());
+      expect(adConfigFixture.articleAdConfiguration, isA<ArticleAdConfiguration>());
+    });
+
+    test('supports value equality', () {
+      final config1 = adConfigFixture.copyWith();
+      final config2 = adConfigFixture.copyWith();
+      expect(config1, equals(config2));
+    });
+
+    test('copyWith returns a new instance with updated values', () {
+      final updatedConfig = adConfigFixture.copyWith(
+        primaryAdPlatform: AdPlatformType.local,
+        feedAdConfiguration: adConfigFixture.feedAdConfiguration.copyWith(enabled: false),
+      );
+
+      expect(updatedConfig.primaryAdPlatform, AdPlatformType.local);
+      expect(updatedConfig.feedAdConfiguration.enabled, isFalse);
+      expect(updatedConfig, isNot(equals(adConfigFixture)));
+    });
+
+    test('copyWith returns same instance if no changes', () {
+      final updatedConfig = adConfigFixture.copyWith();
+      expect(updatedConfig, equals(adConfigFixture));
+    });
+
+    group('fromJson/toJson', () {
+      test('round trip', () {
+        final json = adConfigFixture.toJson();
+        final result = AdConfig.fromJson(json);
+        expect(result, equals(adConfigFixture));
+      });
+    });
+  });
+}
