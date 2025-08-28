f1e3106e895d39a654cd92032196c3795ae68304
fulleni <fulleni@hotmail.com>
2025-08-28 17:10:40 +0100

    feat: add AdPlatformType enum
    - Define a new enum to specify the ad platform used across the application
    - Include options for Google AdMob and local custom ad platform

---
+++ lib/src/enums/ad_platform_type.dart
@@ -0,0 +1,10 @@
+/// {@template ad_platform_type}
+/// Defines the single ad platform to be used across the entire application.
+/// {@endtemplate}
+enum AdPlatformType {
+  /// Google AdMob platform.
+  admob,
+
+  /// Local custom ad platform.
+  local,
+}
