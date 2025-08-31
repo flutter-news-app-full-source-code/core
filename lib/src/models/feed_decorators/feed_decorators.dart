381564f4bfcd02f49722cbcd0cc6389ac45d1833
fulleni <fulleni@hotmail.com>
2025-08-31 10:54:21 +0100

    refactor(models): reorganize local ads classes and update exports
    - Remove local ad exports from feed_decorators.dart
    - Create new local_ads.dart file for local ad exports
    - Move local ad model classes to a new local_ads directory
    - Update models.dart to export new local_ads.dart file

--- lib/src/models/feed_decorators/feed_decorators.dart
+++ lib/src/models/feed_decorators/feed_decorators.dart
@@ -1,7 +1,3 @@
 export 'call_to_action_item.dart';
 export 'content_collection_item.dart';
 export 'local_ad.dart';
-export 'local_banner_ad.dart';
-export 'local_interstitial_ad.dart';
-export 'local_native_ad.dart';
-export 'local_video_ad.dart';
--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -4,6 +4,7 @@ export 'core/core.dart';
 export 'dashboard/dashboard.dart';
 export 'entities/entities.dart';
 export 'feed_decorators/feed_decorators.dart';
+export 'local_ads/local_ads.dart';
 export 'query/query.dart';
 export 'responses/responses.dart';
 export 'user_preferences/user_preferences.dart';
