6a956dfd6ce07745bd0b041b0f194658d0c8ebdb
fulleni <fulleni@hotmail.com>
2025-09-01 08:02:39 +0100

    feat(LocalInterstitialAd): add createdAt, updatedAt, and status fields
    - Add createdAt and updatedAt fields for tracking ad creation and update timestamps
    - Add status field to indicate the current status of the ad
    - Update props list to include new fields
    - Modify copyWith method to support new fields
    - Import necessary enums and utils for new fields

--- lib/src/models/local_ads/local_interstitial_ad.dart
+++ lib/src/models/local_ads/local_interstitial_ad.dart
@@ -1,4 +1,6 @@
 import 'package:core/core.dart';
+import 'package:core/src/enums/enums.dart';
+import 'package:core/src/utils/utils.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
 
@@ -18,6 +20,9 @@ class LocalInterstitialAd extends LocalAd {
     required this.id,
     required this.imageUrl,
     required this.targetUrl,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.status,
   }) : super(adType: 'interstitial');
 
   /// Creates a [LocalInterstitialAd] from JSON data.
@@ -25,6 +30,7 @@ class LocalInterstitialAd extends LocalAd {
       _$LocalInterstitialAdFromJson(json);
 
   /// Unique identifier for the local interstitial ad.
+  @override
   final String id;
 
   /// The URL of the image for the local interstitial ad.
@@ -32,6 +38,17 @@ class LocalInterstitialAd extends LocalAd {
 
   /// The URL to navigate to when the local interstitial ad is clicked.
   final String targetUrl;
+
+  /// The creation timestamp of the local interstitial ad.
+  @JsonKey(fromJson: dateTimeFromJson, toJson: dateTimeToJson)
+  final DateTime createdAt;
+
+  /// The last update timestamp of the local interstitial ad.
+  @JsonKey(fromJson: dateTimeFromJson, toJson: dateTimeToJson)
+  final DateTime updatedAt;
+
+  /// The current status of the local interstitial ad.
+  final ContentStatus status;
 
   Map<String, dynamic> toJson() {
     final json = _$LocalInterstitialAdToJson(this);
@@ -41,7 +58,16 @@ class LocalInterstitialAd extends LocalAd {
   }
 
   @override
-  List<Object?> get props => [id, imageUrl, targetUrl, adType, type];
+  List<Object?> get props => [
+    id,
+    imageUrl,
+    targetUrl,
+    createdAt,
+    updatedAt,
+    status,
+    adType,
+    type,
+  ];
 
   /// Creates a copy of this [LocalInterstitialAd] but with the given fields
   /// replaced with the new values.
@@ -49,11 +75,17 @@ class LocalInterstitialAd extends LocalAd {
     String? id,
     String? imageUrl,
     String? targetUrl,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    ContentStatus? status,
   }) {
     return LocalInterstitialAd(
       id: id ?? this.id,
       imageUrl: imageUrl ?? this.imageUrl,
       targetUrl: targetUrl ?? this.targetUrl,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      status: status ?? this.status,
     );
   }
 }
