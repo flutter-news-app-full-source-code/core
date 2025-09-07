7b3aefb429d325b3ac2b0bfc0aa0992da4d9e7cb
fulleni <fulleni@hotmail.com>
2025-09-07 08:50:57 +0100

    feat: add BannerAdShape enum
    - Defines the visual shape of a banner advertisement
    - Includes two values: square and rectangle

---
+++ lib/src/enums/banner_ad_shape.dart
@@ -0,0 +1,10 @@
+/// {@template banner_ad_shape}
+/// Defines the visual shape of a banner advertisement.
+/// {@endtemplate}
+enum BannerAdShape {
+  /// A square banner ad shape.
+  square,
+
+  /// A rectangular banner ad shape.
+  rectangle,
+}
