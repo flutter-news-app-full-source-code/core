d81f1031e1ffa57e749511d69bbcf58807edbd80
fulleni <fulleni@hotmail.com>
2025-11-05 10:42:27 +0100

    chore: misc

---
+++ lib/src/models/models.dart
@@ -0,0 +1,12 @@
+export 'auth/auth.dart';
+export 'config/config.dart';
+export 'core/core.dart';
+export 'dashboard/dashboard.dart';
+export 'entities/entities.dart';
+export 'feed_decorators/feed_decorators.dart';
+export 'push_notifications/push_notifications.dart';
+export 'query/query.dart';
+export 'responses/responses.dart';
+export 'user_preferences/user_preferences.dart';
+export 'user_presets/user_presets.dart';
+export 'user_settings/user_settings.dart';
--- lib/src/models/push_notifications/push_notifications.dart
+++ lib/src/models/push_notifications/push_notifications.dart
@@ -1,4 +1,3 @@
-export 'notification_subscription.dart';
 export 'push_notification_device.dart';
 export 'push_notification_payload.dart';
 export 'push_notification_subscription.dart';
