f1a67aa22f702c4ab7042abfe8b5e45a50d441cb
fulleni <fulleni@hotmail.com>
2025-11-25 03:42:17 +0100

    fix(remote_configs): update ad platform configuration
    - Replace AdPlatformType.demo with AdPlatformType.admob as the primary ad platform
    - Remove AdPlatformType.demo from platformAdIdentifiers map
    - Update AdPlatformType.admob identifiers with valid test ad IDs

--- lib/src/fixtures/remote_configs.dart
+++ lib/src/fixtures/remote_configs.dart
@@ -70,17 +70,12 @@ final remoteConfigsFixturesData = <RemoteConfig>[
     features: const FeaturesConfig(
       ads: AdConfig(
         enabled: true,
-        primaryAdPlatform: AdPlatformType.demo,
+        primaryAdPlatform: AdPlatformType.admob,
         platformAdIdentifiers: {
           AdPlatformType.admob: AdPlatformIdentifiers(
             nativeAdId: 'ca-app-pub-3940256099942544/2247696110',
             bannerAdId: 'ca-app-pub-3940256099942544/6300978111',
             interstitialAdId: 'ca-app-pub-3940256099942544/1033173712',
-          ),
-          AdPlatformType.demo: AdPlatformIdentifiers(
-            nativeAdId: '_',
-            bannerAdId: '_',
-            interstitialAdId: '_',
           ),
         },
         feedAdConfiguration: FeedAdConfiguration(
