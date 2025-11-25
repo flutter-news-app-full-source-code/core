9c0cba00aa4d3ccc8f668b582308eaed7c966538
fulleni <fulleni@hotmail.com>
2025-11-25 03:25:45 +0100

    build(serialization): generate

--- lib/src/models/config/general_app_config.g.dart
+++ lib/src/models/config/general_app_config.g.dart
@@ -6,11 +6,18 @@ part of 'general_app_config.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-GeneralAppConfig _$GeneralAppConfigFromJson(Map<String, dynamic> json) =>
-    $checkedCreate('GeneralAppConfig', json, ($checkedConvert) {
-      final val = GeneralAppConfig();
-      return val;
-    });
+GeneralAppConfig _$GeneralAppConfigFromJson(
+  Map<String, dynamic> json,
+) => $checkedCreate('GeneralAppConfig', json, ($checkedConvert) {
+  final val = GeneralAppConfig(
+    termsOfServiceUrl: $checkedConvert('termsOfServiceUrl', (v) => v as String),
+    privacyPolicyUrl: $checkedConvert('privacyPolicyUrl', (v) => v as String),
+  );
+  return val;
+});
 
 Map<String, dynamic> _$GeneralAppConfigToJson(GeneralAppConfig instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'termsOfServiceUrl': instance.termsOfServiceUrl,
+      'privacyPolicyUrl': instance.privacyPolicyUrl,
+    };
