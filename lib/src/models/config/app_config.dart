516b37ba6d49837e8083cb0c23dac79be81fc54b
fulleni <fulleni@hotmail.com>
2025-11-25 03:01:39 +0100

    feat(core): add AppConfig model
    - Create a new AppConfig class to hold application-level configurations
    - Include MaintenanceConfig, UpdateConfig, and GeneralAppConfig
    - Implement JSON serialization and deserialization
    - Use Equatable for value comparison

---
+++ lib/src/models/config/app_config.dart
@@ -0,0 +1,41 @@
+import 'package:core/src/models/config/general_app_config.dart';
+import 'package:core/src/models/config/maintenance_config.dart';
+import 'package:core/src/models/config/update_config.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part 'app_config.g.dart';
+
+/// {@template app_config}
+/// A container for all application-level configurations.
+/// {@endtemplate}
+@immutable
+@JsonSerializable(explicitToJson: true, includeIfNull: true, checked: true)
+class AppConfig extends Equatable {
+  /// {@macro app_config}
+  const AppConfig({
+    required this.maintenance,
+    required this.update,
+    required this.general,
+  });
+
+  /// Creates an [AppConfig] from JSON data.
+  factory AppConfig.fromJson(Map<String, dynamic> json) =>
+      _$AppConfigFromJson(json);
+
+  /// Configuration for maintenance mode.
+  final MaintenanceConfig maintenance;
+
+  /// Configuration for application updates.
+  final UpdateConfig update;
+
+  /// General application settings.
+  final GeneralAppConfig general;
+
+  /// Converts this [AppConfig] instance to JSON data.
+  Map<String, dynamic> toJson() => _$AppConfigToJson(this);
+
+  @override
+  List<Object> get props => [maintenance, update, general];
+}
