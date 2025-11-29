d8b26c620489bff280e0c8b6bddb6daed2c89cd6
fulleni <fulleni@hotmail.com>
2025-11-29 10:21:02 +0100

    test(core): add ReportingConfig model tests
    - Add unit tests for ReportingConfig model
    - Verify instantiation, equality, JSON serialization/deserialization
    - Test copyWith functionality

---
+++ test/src/models/config/reporting_config_test.dart
@@ -0,0 +1,42 @@
+import 'package:core/core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ReportingConfig', () {
+    final reportingConfigFixture =
+        remoteConfigsFixturesData.first.features.community.reporting;
+    final json = reportingConfigFixture.toJson();
+
+    test('can be instantiated', () {
+      expect(reportingConfigFixture, isA<ReportingConfig>());
+    });
+
+    test('supports value equality', () {
+      final anotherConfig =
+          remoteConfigsFixturesData.first.features.community.reporting;
+      expect(reportingConfigFixture, equals(anotherConfig));
+    });
+
+    test('can be created from JSON', () {
+      final fromJson = ReportingConfig.fromJson(json);
+      expect(fromJson, equals(reportingConfigFixture));
+    });
+
+    test('can be converted to JSON', () {
+      final toJson = reportingConfigFixture.toJson();
+      expect(toJson, equals(json));
+    });
+
+    test('copyWith creates a copy with updated values', () {
+      final updatedConfig = reportingConfigFixture.copyWith(
+        headlineReportingEnabled: false,
+      );
+
+      expect(updatedConfig.headlineReportingEnabled, isFalse);
+      expect(
+        updatedConfig,
+        isNot(equals(reportingConfigFixture)),
+      );
+    });
+  });
+}
