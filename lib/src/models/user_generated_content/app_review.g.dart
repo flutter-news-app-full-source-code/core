0608c5138c2c17efc5d2679c535e7e7ff2645722
fulleni <fulleni@hotmail.com>
2025-11-30 12:47:05 +0100

    build(serialization): sync

--- lib/src/models/user_generated_content/app_review.g.dart
+++ lib/src/models/user_generated_content/app_review.g.dart
@@ -6,48 +6,41 @@ part of 'app_review.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-AppReview _$AppReviewFromJson(Map<String, dynamic> json) =>
-    $checkedCreate('AppReview', json, ($checkedConvert) {
-      final val = AppReview(
-        id: $checkedConvert('id', (v) => v as String),
-        userId: $checkedConvert('userId', (v) => v as String),
-        initialFeedback: $checkedConvert(
-          'initialFeedback',
-          (v) => $enumDecode(_$InitialAppReviewFeedbackEnumMap, v),
-        ),
-        createdAt: $checkedConvert(
-          'createdAt',
-          (v) => dateTimeFromJson(v as String?),
-        ),
-        updatedAt: $checkedConvert(
-          'updatedAt',
-          (v) => dateTimeFromJson(v as String?),
-        ),
-        negativeFeedbackHistory: $checkedConvert(
-          'negativeFeedbackHistory',
-          (v) =>
-              (v as List<dynamic>?)
-                  ?.map(
-                    (e) => NegativeFeedback.fromJson(e as Map<String, dynamic>),
-                  )
-                  .toList() ??
-              const [],
-        ),
-        wasStoreReviewRequested: $checkedConvert(
-          'wasStoreReviewRequested',
-          (v) => v as bool? ?? false,
-        ),
-      );
-      return val;
-    });
+AppReview _$AppReviewFromJson(
+  Map<String, dynamic> json,
+) => $checkedCreate('AppReview', json, ($checkedConvert) {
+  final val = AppReview(
+    id: $checkedConvert('id', (v) => v as String),
+    userId: $checkedConvert('userId', (v) => v as String),
+    initialFeedback: $checkedConvert(
+      'initialFeedback',
+      (v) => $enumDecode(_$InitialAppReviewFeedbackEnumMap, v),
+    ),
+    createdAt: $checkedConvert('createdAt', (v) => DateTime.parse(v as String)),
+    updatedAt: $checkedConvert('updatedAt', (v) => DateTime.parse(v as String)),
+    negativeFeedbackHistory: $checkedConvert(
+      'negativeFeedbackHistory',
+      (v) =>
+          (v as List<dynamic>?)
+              ?.map((e) => NegativeFeedback.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+    ),
+    wasStoreReviewRequested: $checkedConvert(
+      'wasStoreReviewRequested',
+      (v) => v as bool? ?? false,
+    ),
+  );
+  return val;
+});
 
 Map<String, dynamic> _$AppReviewToJson(AppReview instance) => <String, dynamic>{
   'id': instance.id,
   'userId': instance.userId,
   'initialFeedback':
       _$InitialAppReviewFeedbackEnumMap[instance.initialFeedback]!,
-  'createdAt': dateTimeToJson(instance.createdAt),
-  'updatedAt': dateTimeToJson(instance.updatedAt),
+  'createdAt': instance.createdAt.toIso8601String(),
+  'updatedAt': instance.updatedAt.toIso8601String(),
   'wasStoreReviewRequested': instance.wasStoreReviewRequested,
   'negativeFeedbackHistory': instance.negativeFeedbackHistory
       .map((e) => e.toJson())
