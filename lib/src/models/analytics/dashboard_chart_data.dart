aefe6656e74140d963fa7ef0d9b23d3053060580
fulleni <fulleni@hotmail.com>
2025-12-15 11:20:33 +0100

    feat(analytics): add DashboardChartData model
    - Create a new model for storing pre-aggregated time-series data for dashboard charts
    - Include unique ID and list of DataPoint objects
    - Implement JSON serialization and deserialization

---
+++ lib/src/models/analytics/dashboard_chart_data.dart
@@ -0,0 +1,36 @@
+import 'package:core/src/models/analytics/data_point.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part 'dashboard_chart_data.g.dart';
+
+/// {@template dashboard_chart_data}
+/// A model for storing pre-aggregated time-series data for a dashboard chart.
+///
+/// This model holds a list of [DataPoint] objects and is identified by a
+/// unique ID, allowing it to be stored and fetched from the database.
+/// {@endtemplate}
+@immutable
+@JsonSerializable(explicitToJson: true, includeIfNull: true, checked: true)
+class DashboardChartData extends Equatable {
+  /// {@macro dashboard_chart_data}
+  const DashboardChartData({required this.id, required this.dataPoints});
+
+  /// Creates a [DashboardChartData] from JSON data.
+  factory DashboardChartData.fromJson(Map<String, dynamic> json) =>
+      _$DashboardChartDataFromJson(json);
+
+  /// A unique identifier for the chart data
+  /// (e.g., 'content_management_views_monthly').
+  final String id;
+
+  /// A list of data points representing the time-series data.
+  final List<DataPoint> dataPoints;
+
+  /// Converts this [DashboardChartData] instance to JSON data.
+  Map<String, dynamic> toJson() => _$DashboardChartDataToJson(this);
+
+  @override
+  List<Object> get props => [id, dataPoints];
+}
