8434163fde806f113f7e19d79fe66db8abb3aef7
fulleni <fulleni@hotmail.com>
2025-12-16 07:45:22 +0100

    test(enums): add AnalyticsEvent enum tests
    - Add unit tests for AnalyticsEvent enum
    - Verify the number of enum values
    - Check string names of enum members
    - Test creation

---
+++ test/src/enums/analytics_event_test.dart
@@ -0,0 +1,35 @@
+import 'package:core/src/enums/analytics_event.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('AnalyticsEvent', () {
+    test('has correct number of values', () {
+      // This test is fragile and will need updates if the enum changes.
+      expect(AnalyticsEvent.values.length, 38);
+    });
+
+    test('has correct string names', () {
+      expect(AnalyticsEvent.userRegistered.name, 'userRegistered');
+      expect(
+        AnalyticsEvent.limitExceededCtaClicked.name,
+        'limitExceededCtaClicked',
+      );
+      expect(
+        AnalyticsEvent.headlineFilterCreated.name,
+        'headlineFilterCreated',
+      );
+      expect(AnalyticsEvent.sourceFilterCreated.name, 'sourceFilterCreated');
+    });
+
+    test('can be created from string names', () {
+      expect(
+        AnalyticsEvent.values.byName('userRegistered'),
+        AnalyticsEvent.userRegistered,
+      );
+      expect(
+        AnalyticsEvent.values.byName('headlineFilterCreated'),
+        AnalyticsEvent.headlineFilterCreated,
+      );
+    });
+  });
+}
