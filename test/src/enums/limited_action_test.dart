a96823c9a4a13cb8ed1d92403a1556fdefd42afb
fulleni <fulleni@hotmail.com>
2025-12-16 07:47:11 +0100

    test(core): add LimitedAction enum tests
    - Add unit tests for LimitedAction enum
    - Verify the number of enum values
    - Check string names of enum members
    - Test creation of enum from string names

---
+++ test/src/enums/limited_action_test.dart
@@ -0,0 +1,27 @@
+import 'package:core/src/enums/limited_action.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('LimitedAction', () {
+    test('has correct number of values', () {
+      // This test is fragile and will need updates if the enum changes.
+      expect(LimitedAction.values.length, 10);
+    });
+
+    test('has correct string names', () {
+      expect(LimitedAction.bookmarkHeadline.name, 'bookmarkHeadline');
+      expect(LimitedAction.submitReport.name, 'submitReport');
+    });
+
+    test('can be created from string names', () {
+      expect(
+        LimitedAction.values.byName('bookmarkHeadline'),
+        LimitedAction.bookmarkHeadline,
+      );
+      expect(
+        LimitedAction.values.byName('submitReport'),
+        LimitedAction.submitReport,
+      );
+    });
+  });
+}
