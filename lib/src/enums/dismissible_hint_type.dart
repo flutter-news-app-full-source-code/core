27a0f3dac159f81d09cacf7dfee2163b4b5e1b6a
fulleni <fulleni@hotmail.com>
2025-12-22 17:08:56 +0100

    feat: add DismissibleHintType enum for tracking dismissed hints
    - Introduce new enum to define types of dismissible UI hints or "coach marks"
    - Includes hints for main feed filtering functionality and saving articles

---
+++ lib/src/enums/dismissible_hint_type.dart
@@ -0,0 +1,11 @@
+/// {@template dismissible_hint_type}
+/// Defines the specific types of contextual UI hints or "coach marks" that a
+/// user can dismiss.
+/// {@endtemplate}
+enum DismissibleHintType {
+  /// A hint explaining how to use the main feed's filtering functionality.
+  feedFilterTooltip,
+
+  /// A hint explaining how to save an article for later reading.
+  saveArticleTooltip,
+}
