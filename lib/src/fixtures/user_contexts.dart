e425fc170dbff0102f5311bd6cadb4e5a6da7e76
fulleni <fulleni@hotmail.com>
2025-12-22 17:09:42 +0100

    chore: add fixture

---
+++ lib/src/fixtures/user_contexts.dart
@@ -0,0 +1,25 @@
+import 'package:core/src/enums/feed_decorator_type.dart';
+import 'package:core/src/fixtures/users.dart';
+import 'package:core/src/models/auth/user_context.dart';
+import 'package:core/src/models/auth/user_feed_decorator_status.dart';
+
+final _defaultFeedDecoratorStatus = {
+  for (var type in FeedDecoratorType.values)
+    type: const UserFeedDecoratorStatus(isCompleted: false),
+};
+
+/// Generates a list of [UserContext] fixtures corresponding to the
+/// [usersFixturesData].
+///
+/// Each user in the system gets a default context initialized with
+/// empty/false states for all feed decorators.
+final List<UserContext> userContextsFixturesData = usersFixturesData
+    .map(
+      (user) => UserContext(
+        userId: user.id,
+        feedDecoratorStatus: _defaultFeedDecoratorStatus,
+        hasCompletedOnboarding: true,
+        lastSeenAppVersion: '1.0.0',
+      ),
+    )
+    .toList();
