2a13e199dfcf8065428ecda0d972b75d3dfb0324
fulleni <fulleni@hotmail.com>
2025-12-22 13:49:22 +0100

    refactor(subscription): update SubscriptionConfig to use PlanDetails
    - Replace List<SubscriptionPlan> with individual PlanDetails for monthly and annual plans
    - Update class properties, constructor, toJson method, and copyWith method
    - Improve code structure and prepare for implementing restore purchases feature

--- lib/src/models/config/subscription_config.dart
+++ lib/src/models/config/subscription_config.dart
@@ -1,5 +1,5 @@
 import 'package:core/src/enums/subscription/store_provider.dart';
-import 'package:core/src/models/subscription/subscription_plan.dart';
+import 'package:core/src/models/subscription/plan_details.dart';
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
@@ -17,7 +17,8 @@ class SubscriptionConfig extends Equatable {
   const SubscriptionConfig({
     required this.enabled,
     required this.enabledProviders,
-    required this.plans,
+    required this.monthlyPlan,
+    required this.annualPlan,
   });
 
   /// Creates a [SubscriptionConfig] from JSON data.
@@ -31,26 +32,32 @@ class SubscriptionConfig extends Equatable {
   /// client should initialize and use.
   final List<StoreProvider> enabledProviders;
 
-  /// The list of available subscription plans (the product catalog).
-  final List<SubscriptionPlan> plans;
+  /// Configuration details for the monthly subscription plan.
+  final PlanDetails monthlyPlan;
+
+  /// Configuration details for the annual subscription plan.
+  final PlanDetails annualPlan;
 
   /// Converts this [SubscriptionConfig] instance to JSON data.
   Map<String, dynamic> toJson() => _$SubscriptionConfigToJson(this);
 
   @override
-  List<Object?> get props => [enabled, enabledProviders, plans];
+  List<Object?> get props =>
+      [enabled, enabledProviders, monthlyPlan, annualPlan];
 
   /// Creates a copy of this [SubscriptionConfig] but with the given fields
   /// replaced with the new values.
   SubscriptionConfig copyWith({
     bool? enabled,
     List<StoreProvider>? enabledProviders,
-    List<SubscriptionPlan>? plans,
+    PlanDetails? monthlyPlan,
+    PlanDetails? annualPlan,
   }) {
     return SubscriptionConfig(
       enabled: enabled ?? this.enabled,
       enabledProviders: enabledProviders ?? this.enabledProviders,
-      plans: plans ?? this.plans,
+      monthlyPlan: monthlyPlan ?? this.monthlyPlan,
+      annualPlan: annualPlan ?? this.annualPlan,
     );
   }
 }
