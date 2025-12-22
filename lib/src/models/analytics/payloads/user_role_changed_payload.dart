e8a0827650caa6a850c746c5e79029af6251f335
fulleni <fulleni@hotmail.com>
2025-12-22 11:15:02 +0100

    refactor(analytics): update user role changed payload
    - Replace AppUserRole with UserRole
    - Update import path forref UserRole enumactor

--- lib/src/models/analytics/payloads/user_role_changed_payload.dart
+++ lib/src/models/analytics/payloads/user_role_changed_payload.dart
@@ -1,4 +1,4 @@
-import 'package:core/src/enums/app_user_role.dart';
+import 'package:core/src/enums/user_role.dart';
 import 'package:core/src/models/analytics/analytics_event_payload.dart';
 import 'package:core/src/models/analytics/analytics_parameter_keys.dart';
 
@@ -10,10 +10,10 @@ class UserRoleChangedPayload extends AnalyticsEventPayload {
   const UserRoleChangedPayload({required this.fromRole, required this.toRole});
 
   /// The user's role before the change.
-  final AppUserRole fromRole;
+  final UserRole fromRole;
 
   /// The user's new role after the change.
-  final AppUserRole toRole;
+  final UserRole toRole;
 
   @override
   Map<String, dynamic> toMap() {
