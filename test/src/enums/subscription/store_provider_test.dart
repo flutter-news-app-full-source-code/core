1e81bb5a9b9f9fad400ef462308a4652208d71ac
fulleni <fulleni@hotmail.com>
2025-12-26 07:04:53 +0100

    test(enums): update StoreProvider tests after enum value removal
    - Adjust test cases to reflect the removal of StoreProvider.stripe
    - Update the expected length of StoreProvider.values from 3 to 2
    - Remove tests related to StoreProvider.stripe serialization and deserialization

--- test/src/enums/subscription/store_provider_test.dart
+++ test/src/enums/subscription/store_provider_test.dart
@@ -4,20 +4,18 @@ import 'package:test/test.dart';
 void main() {
   group('StoreProvider', () {
     test('has correct number of values', () {
-      expect(StoreProvider.values.length, 3);
+      expect(StoreProvider.values.length, 2);
     });
 
     group('serialization', () {
       test('uses correct string values for json serialization', () {
         expect(StoreProvider.apple.name, 'apple');
         expect(StoreProvider.google.name, 'google');
-        expect(StoreProvider.stripe.name, 'stripe');
       });
 
       test('can be created from string value for json deserialization', () {
         expect(StoreProvider.values.byName('apple'), StoreProvider.apple);
         expect(StoreProvider.values.byName('google'), StoreProvider.google);
-        expect(StoreProvider.values.byName('stripe'), StoreProvider.stripe);
       });
 
       test('throws ArgumentError for invalid string value', () {
