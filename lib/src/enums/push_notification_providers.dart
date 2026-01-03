2e551d32886511d24c1b3d2bcf460c5da6953b23
fulleni <fulleni@hotmail.com>
2026-01-03 10:22:38 +0100

    feat(push_notifications): add json serialization for PushNotificationProviders
    - Import json_annotation package
    - Annotate enum with @JsonEnum()
    - Add @JsonValue() annotations to enum values for custom JSON keys

--- lib/src/enums/push_notification_providers.dart
+++ lib/src/enums/push_notification_providers.dart
@@ -1,3 +1,5 @@
+import 'package:json_annotation/json_annotation.dart';
+
 /// {@template push_notification_providers}
 /// Defines the available push notification service providers.
 ///
@@ -5,10 +7,13 @@
 /// notification and allows devices to register themselves with the provider
 /// they are configured to use.
 /// {@endtemplate}
+@JsonEnum()
 enum PushNotificationProviders {
   /// Firebase Cloud Messaging.
+  @JsonValue('firebase')
   firebase,
 
   /// OneSignal.
+  @JsonValue('oneSignal')
   oneSignal,
 }
