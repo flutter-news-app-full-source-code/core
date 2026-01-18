0814917f715a65cea44937c7d63bd7ca5e51c168
Fulleni Flen <fulleni@hotmail.com>
2026-01-18 09:27:32 +0100

    refactor(config): flatten user limits to guest/standard

--- lib/src/models/config/user_limits_config.dart
+++ lib/src/models/config/user_limits_config.dart
@@ -1,4 +1,4 @@
-import 'package:core/src/enums/subscription/access_tier.dart' show AccessTier;
+import 'package:core/core.dart';
 import 'package:core/src/models/config/saved_filter_limits.dart';
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
