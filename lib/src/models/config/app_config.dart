d4bbfccfba24468fbaf9fc48ca9e184236aab9d1
Fulleni Flen <fulleni@hotmail.com>
2026-02-12 11:36:57 +0100

    refactor(models): remove InitialPersonalizationConfig from AppConfig
    - Remove InitialPersonalizationConfig import and property from AppConfig class
    - Update toJson and props methods to exclude initialPersonalization
    - Simplify copyWith method by removing initialPersonalization parameter

--- lib/src/models/config/app_config.dart
+++ lib/src/models/config/app_config.dart
@@ -1,5 +1,4 @@
 import 'package:core/src/models/config/general_app_config.dart';
-import 'package:core/src/models/config/initial_personalization_config.dart';
 import 'package:core/src/models/config/maintenance_config.dart';
 import 'package:core/src/models/config/update_config.dart';
 import 'package:equatable/equatable.dart';
@@ -19,7 +18,6 @@ class AppConfig extends Equatable {
     required this.maintenance,
     required this.update,
     required this.general,
-    required this.initialPersonalization,
   });
 
   /// Creates an [AppConfig] from JSON data.
@@ -35,19 +33,11 @@ class AppConfig extends Equatable {
   /// General application settings.
   final GeneralAppConfig general;
 
-  /// Configuration for initial personalization flow.
-  final InitialPersonalizationConfig initialPersonalization;
-
   /// Converts this [AppConfig] instance to JSON data.
   Map<String, dynamic> toJson() => _$AppConfigToJson(this);
 
   @override
-  List<Object> get props => [
-    maintenance,
-    update,
-    general,
-    initialPersonalization,
-  ];
+  List<Object> get props => [maintenance, update, general];
 
   /// Creates a copy of this [AppConfig] but with the given fields
   /// replaced with the new values.
@@ -55,14 +45,11 @@ class AppConfig extends Equatable {
     MaintenanceConfig? maintenance,
     UpdateConfig? update,
     GeneralAppConfig? general,
-    InitialPersonalizationConfig? initialPersonalization,
   }) {
     return AppConfig(
       maintenance: maintenance ?? this.maintenance,
       update: update ?? this.update,
       general: general ?? this.general,
-      initialPersonalization:
-          initialPersonalization ?? this.initialPersonalization,
     );
   }
 }
