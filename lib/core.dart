b2a0852cc0d5afec99b6946c8b16f01c5a9f18b5
Fulleni Flen <fulleni@hotmail.com>
2026-02-16 06:28:53 +0100

    chore: barrels

--- lib/core.dart
+++ lib/core.dart
@@ -1,7 +1,9 @@
 /// Shared utilities and models.
 library;
 
+export 'src/clients/clients.dart';
 export 'src/enums/enums.dart';
 export 'src/exceptions/exceptions.dart';
 export 'src/fixtures/fixtures.dart';
 export 'src/models/models.dart';
+export 'src/repositories/repositories.dart';
